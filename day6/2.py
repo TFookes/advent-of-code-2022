70e951219283f4c1a0fae9a6be949cdd575cb003
Thomas Fookes <thomas.fookes@imanage.com>
2022-12-06 09:19:49 +0000

    day 6

---
+++ day6/2.py
@@ -0,0 +1,23 @@
+def find_marker_pos(chars):
+    buffer = []
+
+    for pos, char in enumerate(chars):
+        if check_ahead(13, pos, chars): return pos + 14
+
+
+def check_ahead(num_to_look, pos, chars):
+    buffer = [chars[pos]]
+    for i in range(1, num_to_look + 1):
+        if chars[pos + i] in buffer: return False
+        buffer.append(chars[pos + i])
+
+    print(buffer)
+    return True
+
+
+if __name__ == "__main__":
+    with open("./inputs.txt", "r") as inputs:
+        lines = inputs.readlines()
+        lines = [line.strip() for line in lines]
+        for line in lines:
+            print(find_marker_pos(line))
